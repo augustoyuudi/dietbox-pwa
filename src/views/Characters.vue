1119c78cd1079543e589c4fb1a8440648f8f3ddb
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-08-05 01:57:56 -0300

    WIP: characters page

---
+++ src/views/Characters.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .characters-view
+    ul
+      li(
+        v-for="character in characters"
+        :key="character.name"
+      ) {{ character.name }}
+    p {{ characters[0] }}
+</template>
+
+<script>
+export default {
+  name: 'CharactersView',
+  created() {
+    this.fetchCharacters();
+  },
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  methods: {
+    async fetchCharacters() {
+      const url = 'https://swapi.dev/api/people/?page=2';
+      const response = await fetch(url, {
+        method: 'GET',
+      });
+      this.characters = (await response.json()).results;
+    },
+  },
+};
+</script>
