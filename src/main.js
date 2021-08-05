e37e8a68b59e985799dae62ad771c19bf2e1efe1
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-08-05 01:57:02 -0300

    feat: add global reset style

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+// Global styles
+import './styles/global/reset.styl';
+
 Vue.config.productionTip = false;
 
 new Vue({
